ea7ebcf1b129c7246217afc526f3b0b30e964dbb
ClintonSBarnes <clintbarnes8@gmail.com>
2022-07-03 15:25:31 -0500

    Add project files.

---
+++ Safer Number Crunching/Program.cs
@@ -0,0 +1,18 @@
+﻿int num = 0;
+double dub = 0;
+bool boo;
+
+Console.WriteLine("Please enter a number: ");
+
+int.TryParse(Console.ReadLine(), out num);
+
+Console.WriteLine("Please enter a number with a decimal: ");
+double.TryParse(Console.ReadLine(), out dub);
+
+Console.WriteLine("Please enter a True or False: ");
+bool.TryParse(Console.ReadLine(), out boo);
+
+
+Console.WriteLine(num);
+Console.WriteLine(dub);
+Console.WriteLine(boo);
